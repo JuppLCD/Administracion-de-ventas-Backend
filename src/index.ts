f24189fc69ffb89cd2eafeed9728d74b17003fb9
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-16 23:42:48 -0300

    feat: :heavy_plus_sign: Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (_, res) => {
 	res.send('HOLA MUNDO');
 });
-
-const PORT = 8000;
 
 app.listen(PORT, () => {
 	console.log('Applicacion en puerto ' + PORT);
