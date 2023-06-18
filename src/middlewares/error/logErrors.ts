ba8261dffef7056dd2e2d5dd284b4ff86ac4afa7
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-18 19:46:11 -0300

    feat: :goal_net: Show the path where the error occurred and hiding the stack (use only for debugging)

--- src/middlewares/error/logErrors.ts
+++ src/middlewares/error/logErrors.ts
@@ -2,8 +2,9 @@ import { ErrorRequestHandler } from 'express';
 
 export const logErrors: ErrorRequestHandler = (err, req, res, next) => {
 	console.log('################## Error ##################');
+	console.log(`Error path --> ${req.originalUrl}`);
 	console.log(JSON.stringify(err));
-	console.error(err.stack);
+	// console.error(err.stack);
 	console.log('###########################################');
 	next(err);
 };
