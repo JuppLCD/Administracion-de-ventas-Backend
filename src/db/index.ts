3a25fb35abbf4586e166a992d9dd3547ec60494a
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-21 19:03:01 -0300

    feat: :sparkles: Init with connection to DB

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import { sequelize } from './connection';
+
+// Models
+import userSchema from './../models/user';
+const UserModel = userSchema(sequelize);
+
+export default async function connectToDB() {
+	try {
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
+	} catch (err) {
+		console.error('Unable to connect to the database: ', err);
+	}
+}
+
+export { UserModel };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from './app';
+import connectToDB from './db';
 
 app.listen(app.get('port'), () => {
 	console.log(`Server on port ${app.get('port')}`);
+	connectToDB();
 });
