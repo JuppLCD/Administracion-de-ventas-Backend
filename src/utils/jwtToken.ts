5b00161a3a5fb5dff25b0801a5bb39f638fcb341
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-23 23:02:14 -0300

    refactor: :truck: Renaming utility "jwt deCode" to "jwt Tokens" and its functions

---
+++ src/utils/jwtToken.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+import { ENV } from '../config';
+
+import type { IPayloadJWT } from '../types/user.interface';
+
+export async function generateToken(payload: IPayloadJWT) {
+	// Expires in 2 days
+	const expiresIn = 60 * 60 * 20 * 2;
+	return await jwt.sign(payload, ENV.JWT_SECRET_KEY, {
+		expiresIn,
+	});
+}
+
+export async function verifyToken(token: string) {
+	return await jwt.verify(token, ENV.JWT_SECRET_KEY);
+}
