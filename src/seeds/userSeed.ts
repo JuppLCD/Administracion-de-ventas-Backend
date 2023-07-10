b65f09b4ad727d4afabb2ef8f307eb1092a77d94
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-10 18:53:08 -0300

    feat: :seedling: Modifying the seeding of the user model, to create the roles and add the new fields

--- src/seeds/userSeed.ts
+++ src/seeds/userSeed.ts
@@ -1,7 +1,37 @@
-import { UserModel } from '../db';
+import { RoleModel, UserModel } from '../db';
 
 export async function userSeed() {
-	await UserModel.create({ email: 'test@test.com', code: '', expire_code: '' });
-	await UserModel.create({ email: 'test1@test.com', code: '', expire_code: '' });
-	await UserModel.create({ email: 'test2@test.com', code: '', expire_code: '' });
+	await RoleModel.create({ name: 'Admin', description: '' });
+	await RoleModel.create({ name: 'Seller', description: '' });
+
+	await UserModel.create({
+		email: 'test@test.com',
+		role_id: 1,
+		code: '',
+		expire_code: '',
+		address: '',
+		phone: '',
+		document_number: '',
+		document_type: '',
+	});
+	await UserModel.create({
+		email: 'test1@test.com',
+		role_id: 1,
+		code: '',
+		expire_code: '',
+		address: '',
+		phone: '',
+		document_number: '',
+		document_type: '',
+	});
+	await UserModel.create({
+		email: 'test2@test.com',
+		role_id: 2,
+		code: '',
+		expire_code: '',
+		address: '',
+		phone: '',
+		document_number: '',
+		document_type: '',
+	});
 }
