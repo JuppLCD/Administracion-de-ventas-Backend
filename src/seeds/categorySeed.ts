f45c434bbac52472335130654a16d460d4b2c27c
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-11 17:59:44 -0300

    feat: :seedling: Using "faker-js" in the Category model seeding

--- src/seeds/categorySeed.ts
+++ src/seeds/categorySeed.ts
@@ -1,10 +1,16 @@
+import { faker } from '@faker-js/faker';
+
 import { CategoryModel } from '../db';
 
-export async function categorySeed() {
-	await CategoryModel.create({ name: 'Alimentos', description: 'Comida y bebidas' });
-	await CategoryModel.create({ name: 'Ropa', description: 'Todo tipo de prendas' });
-	await CategoryModel.create({
-		name: 'Electrodomesticos',
-		description: 'Cualquier aparato electronico que pueda ser colocado en una casa',
-	});
+export async function categorySeed(categoriesToGenerate: number = 5) {
+	let categoriesNames: Set<string> = new Set();
+
+	while (categoriesNames.size < categoriesToGenerate) {
+		categoriesNames.add(faker.commerce.department());
+	}
+
+	const names = Array.from(new Set(categoriesNames));
+	for (let i = 0; i < categoriesToGenerate; i++) {
+		await CategoryModel.create({ name: names[i], description: faker.lorem.paragraph() });
+	}
 }
