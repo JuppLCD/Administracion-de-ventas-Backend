b058120312a6cd3fb32d356d782fef4ffe505c4a
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-11 17:59:11 -0300

    feat: :seedling: Modifying the Product model seed, for the new property

--- src/seeds/productSeed.ts
+++ src/seeds/productSeed.ts
@@ -3,9 +3,8 @@ import { faker } from '@faker-js/faker';
 import { ProductModel, CategoryModel } from '../db';
 import createCode from '../utils/createCode';
 
-export async function productSeed() {
+export async function productSeed(productsToGenerate: number = 30) {
 	const allCategories = await CategoryModel.findAll();
-	const productsToGenerate = 15;
 
 	for (let i = 0; i < productsToGenerate; i++) {
 		const categoryIndex = Math.floor(Math.random() * allCategories.length);
@@ -15,10 +14,11 @@ export async function productSeed() {
 			category_id: category.id,
 
 			name: faker.commerce.productName(),
+			type: faker.commerce.product(),
 			description: faker.commerce.productDescription(),
 			code: createCode(5),
 			img: 'https://dummyimage.com/300x200/000/fff.png',
-			price: faker.number.float({ min: 10, max: 600, precision: 0.01 }),
+			price: parseFloat(faker.commerce.price()),
 			stock: faker.number.int({ min: 1, max: 50 }),
 		});
 	}
