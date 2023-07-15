2e71c8b32ad6fb78ba99ce8bfabec9fd3f39e135
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-15 16:14:57 -0300

    feat: :label: Types "IVoucherType" and "IVoucherSeries" for interface "IProductEntry"

--- src/types/product_entry.interface.ts
+++ src/types/product_entry.interface.ts
@@ -6,14 +6,17 @@ export interface IProductEntry {
 	user_id: number;
 	provider_id: number;
 
-	voucher_type: string;
-	voucher_series: string;
+	voucher_type: IVoucherType;
+	voucher_series: IVoucherSeries;
 	voucher_number: string;
 	date: string; // 'YYYY-MM-DD hh:mm:ss'
 
 	tax: number;
 	total: number;
 }
+
+export type IVoucherType = 'FACTURA A' | 'FACTURA B' | 'FACTURA C';
+export type IVoucherSeries = 'Serie VD-01' | 'Serie VD-02' | 'Serie C-01' | 'Serie C-02' | 'Serie C-03';
 
 type IProductEntryCreationAttributes = Optional<IProductEntry, 'id'>;
 
