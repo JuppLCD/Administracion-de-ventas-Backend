1161713f539d1019d2dc383c72e31e39b3348278
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-27 19:06:33 -0300

    feat: :heavy_plus_sign: Add morgan

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,22 @@ import { ENV } from './config';
 
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 // Settings
 app.set('port', ENV.PORT);
 
+const corsOptions = {
+	origin: '*',
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // Middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(morgan('dev'));
 
 // Router api
 import { router } from './routes';
