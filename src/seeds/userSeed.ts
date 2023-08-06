5352961b959c1cce3d986d4e5f0d515fbd5d71c6
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-08-06 18:26:26 -0300

    test: :white_check_mark: User Seed

--- src/seeds/userSeed.ts
+++ src/seeds/userSeed.ts
@@ -3,12 +3,12 @@ import { faker } from '@faker-js/faker';
 import { RoleModel, UserModel } from '../db';
 
 export async function userSeed() {
-	await RoleModel.create({ name: 'Admin', description: faker.lorem.paragraph() });
-	await RoleModel.create({ name: 'Seller', description: faker.lorem.paragraph() });
+	const adminRole = await RoleModel.create({ name: 'Admin', description: faker.lorem.paragraph() });
+	const sellerRole = await RoleModel.create({ name: 'Seller', description: faker.lorem.paragraph() });
 
 	await UserModel.create({
 		email: 'test@test.com',
-		role_id: 1,
+		role_id: adminRole.dataValues.id,
 		code: '',
 		expire_code: '',
 		address: faker.location.streetAddress({ useFullAddress: true }),
@@ -19,7 +19,7 @@ export async function userSeed() {
 	});
 	await UserModel.create({
 		email: 'test1@test.com',
-		role_id: 1,
+		role_id: adminRole.dataValues.id,
 		code: '',
 		expire_code: '',
 		address: faker.location.streetAddress({ useFullAddress: true }),
@@ -30,7 +30,7 @@ export async function userSeed() {
 	});
 	await UserModel.create({
 		email: 'test2@test.com',
-		role_id: 2,
+		role_id: sellerRole.dataValues.id,
 		code: '',
 		expire_code: '',
 		address: faker.location.streetAddress({ useFullAddress: true }),
